4c60aa49a94650893a1a5b54674255a6e2c2f804
st10443542-Yasmin-Aloraini <165901566+st10443542-Yasmin-Aloraini@users.noreply.github.com>
2025-05-29 20:42:37 +0200

    Push

---
+++ IceTask3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IceTask3.Models
+{
+    
+        public class Student
+        {
+            public int Id { get; set; }
+
+            public string FullName { get; set; }
+
+            public string Email { get; set; }
+
+            public DateTime EnrollmentDate { get; set; }
+        }
+    
+}
